cb83c57c6aac53d050e39fa68583f8d8a3e7c45d
Sinan Nar <sinan.nar@gmail.com>
2024-03-29 18:09:49 +1300

    save

--- Program.cs
+++ Program.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections.Concurrent;
-Console.WriteLine($"ThreadPool number is : { Environment.ProcessorCount}");
+Console.WriteLine($"ThreadPool number is : {Environment.ProcessorCount}");
 
 AsyncLocal<int> myValue = new();
 for (int i = 0; i < 1000; i++)
 {
     myValue.Value = i;
-    MyThreadPool.QueueUserWorkItem(delegate {
+    MyThreadPool.QueueUserWorkItem(delegate
+    {
         Console.WriteLine(myValue.Value);
         Thread.Sleep(1000);
     });
 }
 Console.ReadLine();
+
+class MyTask
+{
+    private bool _completed;
+    private Exception? _exception;
+    private Action? _continuation;
+    private ExecutionContext? _context;
+
+    public bool IsCompleted
+    {
+        get
+        {
+            return _completed;
+        }
+    }
+
+    public void SetResult() { }
+
+    public void SetException(Exception exception) { }
+
+    public void Wait() { }
+
+    public void ContinueWith(Action action) { }
+
+}
 
 static class MyThreadPool
 {
@@ -24,19 +50,22 @@ static class MyThreadPool
     {
         for (int i = 0; i < Environment.ProcessorCount; i++)
         {
-            new Thread(() => {
-                while(true){
+            new Thread(() =>
+            {
+                while (true)
+                {
                     (Action workItem, ExecutionContext? context) = s_workItems.Take();
-                    if(context is null)
+                    if (context is null)
                     {
                         workItem();
                     }
-                    else{
-                        ExecutionContext.Run(context, state => ((Action)state!).Invoke() , workItem);
+                    else
+                    {
+                        ExecutionContext.Run(context, state => ((Action)state!).Invoke(), workItem);
                     }
                 }
             })
-            {IsBackground = true}.Start();
+            { IsBackground = true }.Start();
         }
     }
 }
