4f7208f819f86db736c3b8012f18211d78141743
Sinan Nar <sinan.nar@gmail.com>
2024-03-29 17:29:23 +1300

    broken threadpool usage

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+#region V1
+for (int i = 0; i < 1000; i++)
+{
+    ThreadPool.QueueUserWorkItem(delegate {
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+    });
+}
+Console.ReadLine();
+/*
+Output
+...
+1000
+1000
+1000
+1000
+1000
+1000
+1000
+1000
+...
+*/ 
+#endregion
